862f5e6fc514e18852a952bbb5eefe4ccfadc8ee
milkyway <hoad3579@gmail.com>
2024-06-19 02:24:27 +0700

    commit-3

--- src/App.tsx
+++ src/App.tsx
@@ -190,7 +190,7 @@ const App: React.FC = () =>{
               </div>
               <div className='flex flex-row grid-cols-4 mt-10 w-3/4'>
                 <div className='border border-gray-600 w-[420px] h-[420px] flex justify-center items-center flex-col rounded-lg shadow-lg shadow-indigo-600'>
-                  <img src="/src/assets/Web.png" alt="website" className='w-96'/>
+                  <img src="/src/assets/web.png" alt="website" className='w-96'/>
                   <div className='mt-5'>
                     <h1 className='font-bold text-2xl text-black'>Website bán hàng</h1>
                     <h2 className='font-serif text-2xl text-black w-96 mt-5'>Website bán hàng là ứng dụng web cho phép người dùng đặt mua và đăng bán các mặt hàng</h2>
