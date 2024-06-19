5561bfbddfec266660c9034a8f5dac71956e0c5f
milkyway <hoad3579@gmail.com>
2024-06-20 01:51:44 +0700

    commit-7

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 
 
 import React, {useEffect, useState} from 'react';
-import { FaGithub } from 'react-icons/fa';
+// import { FaGithub } from 'react-icons/fa';
 import { MdOutlineEmail } from 'react-icons/md';
 import AOS from 'aos';
 import 'aos/dist/aos.css'
 import { useInView } from "react-intersection-observer";
+import { FaGithub } from "react-icons/fa6";
+
 const App: React.FC = () =>{
 
   const [greeting, setGreeting] = useState('');
@@ -184,20 +186,31 @@ const App: React.FC = () =>{
                 </div>
               </div>
             </div>
-            <div id='project' ref={projectRef} className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-400 to-purple-400 text-white">
+            <div id='project' ref={projectRef} className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-400 to-purple-400 ">
               <div className={`font-bold text-4xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold  ${isVisible.project ? '' : 'hidden'}`}>
                 Các dự án đạt được
               </div>
 
                 <div className='flex flex-row grid-cols-4 mt-10 w-3/4'>
-                  <a href='https://github.com/hoad3/Dao_Ngoc_Hoa'>
+                  <a href='https://github.com/hoad3/Shoping-Web' className='m-10'>
                     <button className='transition ease-in duration-300  hover:-translate-y-1 hover:scale-105 hover:shadow-2xl shadow-indigo-600'>
                       <div className='border border-gray-600 w-[420px] h-[420px] flex justify-center items-center flex-col rounded-lg shadow-lg shadow-indigo-600'>
                        <img src="/assets/web.png" alt="website" className='w-96'/>
 
                         <div className='mt-5'>
-                          <h1 className='font-bold text-2xl text-black'>Website bán hàng</h1>
-                          <h2 className='font-serif text-2xl text-black w-96 mt-5'>Website bán hàng là ứng dụng web cho phép người dùng đặt mua và đăng bán các mặt hàng</h2>
+                          <h1 className='font-bold text-2xl text-black'>Shoping Web</h1>
+                          <h2 className='font-serif text-2xl text-black w-96 mt-5'>Shoping Web là ứng dụng web cho phép người dùng đặt mua và đăng bán các mặt hàng</h2>
+                        </div>
+                      </div>
+                    </button>
+                  </a>
+                  <a href='https://github.com/hoad3/NewStalker' className='m-10'>
+                    <button className='transition ease-in duration-300  hover:-translate-y-1 hover:scale-105 hover:shadow-2xl shadow-indigo-600'>
+                      <div className='border border-gray-600 w-[420px] h-[420px] flex justify-center items-center flex-col rounded-lg shadow-lg shadow-indigo-600 overflow-y-scroll '>
+                        <img src="/assets/Summerizer.png" alt="newstalker" className='w-96'/>
+                        <div className='mt-5'>
+                          <h1 className='font-bold text-2xl text-black'>Newtalker</h1>
+                          <h2 className='font-serif text-2xl text-black w-96 mt-5 '>Newtalker là phần mềm crawler dữ liệu một cách tự động từ các bài báo và thực hiện tóm tắt văn bản, thống kê từ khóa trong bài báo đó</h2>
                         </div>
                       </div>
                     </button>
@@ -205,14 +218,93 @@ const App: React.FC = () =>{
                 <div>
 
                 </div>
+              </div>
+              <div className='flex justify-center items-center flex-col'>
+                <span className='font-bold text-4xl h-14 bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent mt-5'>Xem thêm tại</span>
                 <div>
-
+                  <a href='https://github.com/hoad3'>
+                    <FaGithub size='30' className=' m-5 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300 w-11 h-11 rounded-full font-serif' />
+                  </a>
                 </div>
               </div>
             </div>
-            <div id='skill' ref={skillRef} className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-400 to-pink-400 text-white">
-              <h2 className={`text-4xl font-bold ${isVisible.skill ? '' : 'hidden'}`}>The Third slide</h2>
-              <p className={`mt-2 ${isVisible.skill ? '' : 'hidden'}`}>Scroll Down</p>
+            <div id='skill' ref={skillRef} className="h-auto flex flex-col items-center justify-center bg-gradient-to-b from-purple-400 to-pink-400 ">
+              {/*<h2 className={`text-4xl font-bold ${isVisible.skill ? '' : 'hidden'}`}></h2>*/}
+              {/*<p className={`mt-2 ${isVisible.skill ? '' : 'hidden'}`}></p>*/}
+              <div className='flex justify-start items-start w-3/4'>
+                <div className='flex flex-col'>
+                  <h1 className='font-bold text-3xl text-indigo-600'>Về tôi</h1>
+                  <div className='w-7/12'>
+                    <div className='mt-5'>
+                      <span className='font-bold text-3xl text-indigo-600'>Xin chào! tôi tên</span>
+                      <span className='font-bold text-3xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold ml-2'>Đào Ngọc Hòa,</span>
+                      <span className='font-bold text-3xl ml-2 text-indigo-600'>một sinh viên đại học thuộc chuyên ngành</span>
+                      <span className='font-bold text-3xl ml-2 text-indigo-600'>công nghệ phần mềm</span>
+                      <span className='font-bold text-3xl ml-2 text-indigo-600'>tại đại học Tài nguyên và Môi trường Hồ Chí Minh.</span>
+                    </div>
+                    <div className='mt-5'>
+                      <span className='font-bold text-3xl text-indigo-600'>Tôi có kinh nghiệm làm</span>
+                      <span className='font-bold text-3xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold ml-2' >ứng dụng web full-stack.</span>
+                      <span className='font-bold text-3xl ml-2 text-indigo-600'>Ngoài ra tôi cũng có kiến thức và kỹ năng giải quyết vấn đề về  </span>
+                      <span className='font-bold text-3xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold ml-1'>cấu trúc dữ liệu & giải thuật</span>
+                      <span className='font-bold text-3xl ml-2 text-indigo-600'>và các kỹ năng về</span>
+                      <span className='font-bold text-3xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold ml-2'>OPP</span>
+                    </div>
+                    <div className='font-bold text-3xl  bg-gradient-to-r from-indigo-600 to-pink-500 bg-clip-text text-transparent font-bold mt-28 w-auto '>
+                      <span>Kỹ năng</span>
+
+                      <div className='flex'>
+                        <div className='flex flex-col mt-5 w-full justify-center items-center border-t-4 border-indigo-500'>
+                          <div className=' font-bold text-3xl text-indigo-600 flex flex-row h-auto w-auto border-b-4 border-indigo-500 mt-5 h-14'>
+                            <div className='ml-5 w-48'>
+                              <span className='font-bold text-3xl text-indigo-600 '>Ngôn ngữ</span>
+                            </div>
+                            <div className=' font-bold text-3xl text-indigo-600 w-56'>
+                              Công nghệ
+                            </div>
+                            <div className=' font-bold text-3xl text-indigo-600 w-56'>
+                              Cơ sở dữ liệu
+                            </div>
+                            <div className=' font-bold text-3xl text-indigo-600 w-48'>
+                              Công cụ
+                            </div>
+                          </div>
+                          <div className='flex flex-row w-[884px] mt-5'>
+                            <div className='flex flex-col ml-5'>
+                              <img src='/logos/java-ar21.svg' className=' h-32 w-36'/>
+                              <img src='/logos/csharp.svg' className='w-16 mt-5'/>
+                              <img src='/logos/python.svg' className='w-16 mt-5'/>
+                              <img src='/logos/typescript.svg' className='w-16 mt-5'/>
+                              <img src='/logos/javascript.svg' className='w-16 mt-5 mb-10'/>
+                            </div>
+                            <div className='flex flex-col ml-20'>
+                              <img src='/logos/tailwindcss.svg' className='w-16 mt-5'/>
+                              <img src='/logos/dotnet.svg' className='w-16 mt-5'/>
+                              <img src='/logos/react.svg' className='w-16 mt-5'/>
+                              <img src='/logos/vite.svg' className='w-16 mt-5'/>
+                              <img src='/logos/nodedotjs.svg' className='w-16 mt-5'/>
+                            </div >
+                            <div className='flex flex-col ml-40'>
+                              <img src='/logos/postgresql.svg' className='w-16 mt-5'/>
+                              <img src='/logos/mysql.svg' className='w-16 mt-5'/>
+                              <img src='/logos/microsoft-sql-server-logo-svgrepo-com.svg' className='w-20 mt-5'/>
+                            </div>
+                            <div className='flex flex-col ml-40'>
+                              <img src='/logos/jetbrains.svg' className='w-16 mt-5'/>
+                              <img src='/logos/visualstudiocode.svg' className='w-16 mt-5'/>
+                              <img src='/logos/visualstudio.svg' className='w-16 mt-5'/>
+                            </div>
+                          </div>
+                      </div>
+
+                      </div>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
